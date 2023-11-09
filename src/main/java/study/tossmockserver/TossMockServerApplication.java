9ae052ef8376932e4cf7d8658bbd927248bee5fe
hyoguoo <hyoguoo@gmail.com>
2023-11-10 01:54:14 +0900

    init: init project

---
+++ src/main/java/study/tossmockserver/TossMockServerApplication.java
@@ -0,0 +1,13 @@
+package study.tossmockserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TossMockServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TossMockServerApplication.class, args);
+    }
+
+}
